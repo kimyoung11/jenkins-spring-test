6fa704ad8924fabca776cca2f5838c82abc165d2
youngjun <kim_young_@naver.com>
2026-05-14 22:09:12 +0900

    feat: spring boot project is ready

---
+++ src/main/java/jenkins_spring_test/JenkinsSpringTestApplication.java
@@ -0,0 +1,15 @@
+package jenkins_spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("---START---");
+		SpringApplication.run(JenkinsSpringTestApplication.class, args);
+		System.out.println("---END---");
+	}
+
+}
---
+++ src/test/java/jenkins_spring_test/JenkinsSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkins_spring_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
