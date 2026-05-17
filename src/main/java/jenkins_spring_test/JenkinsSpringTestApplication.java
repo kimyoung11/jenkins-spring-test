3d5e66a3ceb4f9a49e4736ae85fd22761ec50099
youngjun <kim_young_@naver.com>
2026-05-17 22:14:26 +0900

    feat: 웹훅 터널 유지 테스트

--- src/main/java/jenkins_spring_test/JenkinsSpringTestApplication.java
+++ src/main/java/jenkins_spring_test/JenkinsSpringTestApplication.java
@@ -9,7 +9,7 @@ public class JenkinsSpringTestApplication {
 	public static void main(String[] args) {
 		System.out.println("---START---");
 		SpringApplication.run(JenkinsSpringTestApplication.class, args);
-		System.out.println("---END---");
+		System.out.println("---END2---");
 	}
 
 }
